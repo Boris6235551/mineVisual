5d9484541785cd7e33eb6480bcfb08d6ded99d1f
Boris6235551 <58598045+Boris6235551@users.noreply.github.com>
2020-12-18 11:36:25 +0300

    first

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+app.allowRendererProcessReuse = false
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            // / allowRunningInsecureContent: true, /
+            nodeIntegration: true,
+            webSecurity: false,
+            enableRemoteModule: true
+        },
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
